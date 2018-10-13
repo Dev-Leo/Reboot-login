9adae37b0fd3d9c4bb97b96e34d48bfe574b4eb7
Dev-Leo <33715512+Dev-Leo@users.noreply.github.com>
2018-10-13 22:32:29 +0800

    Update find.py

--- find.py
+++ find.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 browser.get('https://www.baidu.com')
 browser.find_element_by_id('kw').send_keys('test')
