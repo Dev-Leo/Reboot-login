baa1ecb13963e2088be77e4179f8dba05f756592
Dev-Leo <33715512+Dev-Leo@users.noreply.github.com>
2018-10-13 23:19:11 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,10 +1,14 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 browser.get('https://i.caisan.net/login')
-username = "18888888888"
-password = "88888888"
+username = "18229298314"
+password = "dengziqi520"
 assert "登录 - 彩伞中心" in browser.title
 browser.find_element_by_name("username").send_keys(username)
 browser.find_element_by_name("password").send_keys(password)
 browser.find_element_by_name("password").send_keys(Keys.ENTER)
+time.sleep(5)
+browser.find_element_by_xpath('//div[2]/div/div[2]/ul/li/a').click()
